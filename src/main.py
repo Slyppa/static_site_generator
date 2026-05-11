5b434201d3ff6834353495e294a4ede29ece5bdb
Slyppa <willianhidekis@gmail.com>
2026-05-11 16:51:40 -0300

    fix textnode

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 
 def main():
-    dummy_test = TextNode('This is some anchor text', 'link', 'https://www.boot.dev')
+    dummy_test = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
 
     print(repr(dummy_test))
 
