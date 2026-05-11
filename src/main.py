f9d45cfde8bef89d670ec7db5603c6362bb0ad13
Slyppa <willianhidekis@gmail.com>
2026-05-11 16:40:51 -0300

    text node

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print('hello world')
+from textnode import TextNode
+
+def main():
+    dummy_test = TextNode('This is some anchor text', 'link', 'https://www.boot.dev')
+
+    print(repr(dummy_test))
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,21 +1,23 @@
 from enum import Enum
 
 class TextType(Enum):
-    Plain = 'text'
-    Bold = '**Bold text**'
-    Italic = '_Italic text_'
-    Code = '`Code text`'
-    Link = '[anchor text](url)'
-    Image = '![alt text](url)'
+    Plain = 'plain'
+    Bold = 'bold'
+    Italic = 'italic'
+    Code = 'code'
+    Link = 'link'
+    Image = 'image'
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
-        self.text_type = text_type
         self.url = url
+        if text_type in TextType:
+            self.text_type = text_type
+        else: raise Exception('invalid type')
 
     def __eq__(self, other):
         return self == other
     
     def __repr__(self):
-        return f'TextNode({self.text!r}, {self.text_type!r}, {self.url!r})'
+        return f'TextNode({self.text}, {self.text_type}, {self.url})'
