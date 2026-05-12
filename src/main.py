d31bb1e8d07b36a2e74be7221a2de6bb0494e421
Slyppa <willianhidekis@gmail.com>
2026-05-12 16:38:03 -0300

    copy static

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
+import os
+import shutil
 
-def text_node_to_html_node(TextNode):
+from copystatic import copy_files_recursive
 
-    ...
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    ...
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
-if __name__ == "__main__":
-    main()
+main()
